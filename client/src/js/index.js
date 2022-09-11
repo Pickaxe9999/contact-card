d1cd41eb0b35e2daae01ad3e4fc2a0cadc981717
Jacob H <jacob.hannah164@gmail.com>
2022-09-11 16:56:39 -0400

    created read and write functions for the db

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,7 +3,7 @@ import "./submit";
 import '../css/index.css';
 import {Tooltip, Toast, Popover} from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb } from "./database";
+import { initdb,getDb,postDb } from "./database";
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -11,6 +11,9 @@ import Dog from '../images/dog.png';
 
 window.addEventListener('load', function() {
     initdb();
+    getDb();
+    postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+    getDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
